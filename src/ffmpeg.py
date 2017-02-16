336bdd08fd257f8fd4ad9aaf79dccd57f5dfde97
Victor Lantier <lantier@users.noreply.github.com>
2017-02-16 07:42:45 -0300

    Update ffmpeg.py
    Correct a bug when the script try to convert some IMG to video and the width isn't divisible by two, causing the error: [libx264 @ 0x3a89860] width not divisible by 2 (927x1200)  --- example

--- src/ffmpeg.py
+++ src/ffmpeg.py
@@ -25,7 +25,7 @@ def extract_audio_from_video(video_file, out_file):
 
 
 def create_video_from_image(image, duration, out_file):
-    command = '%s -loop 1 -r 5 -f image2 -i %s -c:v %s -t %f -pix_fmt yuv420p %s 2>> %s' % (
+    command = '%s -loop 1 -r 5 -f image2 -i %s -c:v %s -t %f -pix_fmt yuv420p -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" %s 2>> %s' % (
         FFMPEG, image, VID_ENCODER, duration, out_file, logfile)
     os.system(command)
 
